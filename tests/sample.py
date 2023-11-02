74b78d3c35d1f3303f85ebcad4ad5a1692cdd762
Darshan Mohan Tak <darshan.mohan@juspay.in>
2023-11-02 13:06:41 +0530

    red team basics

---
+++ tests/sample.py
@@ -0,0 +1,12 @@
+import subprocess
+
+def open_terminal():
+    try:
+        # On macOS, the 'open' command can be used to open the Terminal application.
+        subprocess.call(['open', '-a', 'Terminal'])
+    except Exception as e:
+        print(f'An error occurred: {e}')
+
+if __name__ == "__main__":
+    open_terminal()
+
